d42451f31217b91c441dabc9fcded282de32bfdf
MUSTAF <mustafwarsame010@gmail.com>
2025-04-27 19:53:37 -0500

    Update test.televsion.py

--- test.televsion.py
+++ test.televsion.py
@@ -1,55 +1,56 @@
 class Television:
+    
 
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
-
+    def __init__(self) -> None:
+        
         self.__status = False
-        self.__mute = False
+        self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
-    def power(self)-> None:
-        ''' the power fuction'''
+    def power(self) -> None:
+        
         self.__status = not self.__status
+
     def mute(self) -> None:
-        "' the mute fuction when the tv is on'"
-        self.__status
-        self.__mute = not self.__mute
-
-    def channel_up(self)-> None:
-        "' add 1 to the channel fuction '"
+        
         if self.__status:
-            self.__channel += 1
+            self.__muted = not self.__muted
 
-        if self.__channel > Television.MAX_CHANNEL:
-            self.__channel = Television.MIN_CHANNEL
-    def channel_down(self)-> None:
-        "' sebtract one to teh channel fuction'"
+    def channel_up(self) -> None:
+        
         if self.__status:
-            self.__channel -= 1
+            self.__channel = Television.MIN_CHANNEL if self.__channel == Television.MAX_CHANNEL else self.__channel + 1
 
-        if self.__channel < Television.MIN_CHANNEL:
-            self.__channel = Television.MAX_CHANNEL
-    def volume_up(self)-> None:
-        "' increase the volume if muted unmuted first'"
+    def channel_down(self) -> None:
+        
         if self.__status:
-            if self.__mute:
-                self.__mute = False
-            if self.__volume < Television.MAX_VOLUME:
+            self.__channel = Television.MAX_CHANNEL if self.__channel == Television.MIN_CHANNEL else self.__channel - 1
+
+    def volume_up(self) -> None:
+        
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = 1  
+            elif self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
-    def volume_down(self)-> None:
-        "' decreased the volume if muted unmuted first'"
+
+    def volume_down(self) -> None:
+        
         if self.__status:
-            if self.__mute:
-                self.__mute = False
-            if self.__volume > Television.MIN_VOLUME:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = 0  
+            elif self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
-    def __str__(self)-> str:
-
-        return (f"Power = {self.__status}, Channel = {self.__channel}, "
-                f"Volume = {self.__volume}")
+    def __str__(self) -> str:
+        
+        display_volume = 0 if (self.__muted and self.__status) else self.__volume
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {display_volume}"
